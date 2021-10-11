c20d16eaf4811a2301dbaa4be70d3ef50e8e6cc2
jyl7094 <jyl7094@gmail.com>
2021-10-10 23:13:56 -0400

    Add mongoDB

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-//jshint esversion:6
-
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
+const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/postDB');
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  post: {
+    type: String,
+    required: true
+  }
+});
+const Post = mongoose.model('Post', postSchema);
 
 const port = process.env.PORT || 3000;
 
@@ -13,13 +25,19 @@ const homeStartingContent = 'Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = 'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.';
 const contactContent = 'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
 
-const posts = [];
+// const posts = [];
 
 
 app.get('/', (req, res) => {
-  res.render('home', {
-    homeStartingContent: homeStartingContent,
-    posts: posts
+  Post.find({}, (err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('home', {
+        homeStartingContent: homeStartingContent,
+        posts: results
+      });
+    }
   });
 });
 
@@ -39,24 +57,38 @@ app.get('/compose', (req, res) => {
   res.render('compose');
 });
 
-app.get('/posts/:postId', (req, res) => {
-  const postId = req.params.postId.toLowerCase().replace(' ', '-');
-  posts.forEach(post => {
-    if (postId === post.title.toLowerCase().replace(' ', '-')) {
+app.get('/posts/:postTitle', (req, res) => {
+  const postId = req.params.postTitle
+  Post.findOne({_id: postId}, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
       res.render('post', {
-        post: post
+        post: result
       });
     }
   });
+  // posts.forEach(post => {
+  //   if (postId === post.title.toLowerCase().replace(' ', '-')) {
+  //     res.render('post', {
+  //       post: post
+  //     });
+  //   }
+  // });
 });
 
 app.post('/compose', (req, res) => {
-  const post = {
+  const newPost = new Post({
     title: req.body.title,
     post: req.body.post
-  };
-  posts.push(post);
-  res.redirect('/');
+  });
+  newPost.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+    }
+  });
 });
 
 app.listen(port, function () {
